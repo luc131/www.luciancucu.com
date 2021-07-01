068411101840b09844d8fd1b55b7b7f94c8c7780
luc131 <bonvoyage745@gmail.com>
2021-07-01 16:03:51 +0200

    update blog

---
+++ pages/blog/id.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+    <html>
+
+<head>
+
+    <meta name="description" content="Blog post 1">
+
+   
+    <title>
+        Lucian Cucu
+    </title>
+
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: Noto Sans TC, sans-serif;
+        }
+    </style>
+
+    <meta name="viewport" content="width=device-width">
+
+
+</head>
+
+</div>
+</html>
+</template>
+
+
+
+<script>
+export default {
+    layoyt: 'default'
+}
+</script>
+
