679f482effdebe9f67e258a4f4322a2c558b1f6b
luc131 <bonvoyage745@gmail.com>
2021-07-05 16:11:16 +0200

    update luciancucu.com

---
+++ pages/-/_feed/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="max-w-3xl mx-auto bg-white overflow-hidden shadow-md rounded-lg mb-12">
+      <img src="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="" class="w-full h-64 object-cover">
+
+      <div class="p-6">
+        <div>
+          <span class="text-blue-600 text-xs font-medium uppercase">Travel</span>
+          <a href="blogpost1.html" class="block text-gray-800 font-semibold text-2xl mt-2 hover:text-gray-600 hover:underline">I Built A Successful Blog In One Year</a>
+          <p class="text-sm text-gray-600 mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget augue auctor, molestie mauris ut, euismod purus. Praesent sollicitudin lacus in convallis accumsan.
+          </p>
+        </div>
+
+        <div class="mt-4">
+          <div class="flex items-center">
+            <div class="flex items-center">
+              <img src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1008&q=80" class="h-10 object-cover rounded-full" alt="">
+              <a href="#" class="mx-2 text-gray-700 font-semibold hover:underline">Lucian</a>
+            </div>
+            <span class="mx-1 text-gray-600 text-xs">16 June 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="max-w-3xl mx-auto bg-white overflow-hidden shadow-md rounded-lg mb-12">
+      <img src="https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="" class="w-full h-64 object-cover">
+
+      <div class="p-6">
+        <div>
+          <span class="text-blue-600 text-xs font-medium uppercase">Music</span>
+          <a href="blogpost2.html" class="block text-gray-800 font-semibold text-2xl mt-2 hover:text-gray-600 hover:underline">I Built A Successful Blog In One Year</a>
+          <p class="text-sm text-gray-600 mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget augue auctor, molestie mauris ut, euismod purus. Praesent sollicitudin lacus in convallis accumsan.
+          </p>
+        </div>
+
+        <div class="mt-4">
+          <div class="flex items-center">
+            <div class="flex items-center">
+              <img src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" class="h-10 object-cover rounded-full" alt="">
+              Lucian
+            </div>
+            <span class="mx-1 text-gray-600 text-xs">16 June 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,66 @@
 <template>
   <div>
-  <div class="bg-green-900">
-    <h1 class="text-white text-left pl-3 md:pl-20 pt-20 text-3xl">
-      <span class="font-semibold">Lucian Cucu</span>
-    </h1>
-    <h2 class="text-white text-left pl-2 md:pl-20 pt-20">
-      Tirocinio presso <span class="font-semibold">REVAS
-        Societa Benefit</span> Trento.
-      Master in "Esperto in Innovazione Tecnologica applicata".
-    </h2>
-    <h3 class="text-white text-left pl-1.5 md:pl-20 pb-20">
-      Laureato in <span class="font-semibold">Sociologia</span>
-      presso <span class="font-semibold">l'Università di Trento</span>.
-    </h3>
-  </div>
-  <div class="flex-row flex-shrink md:flex">
-    <div class="md:w-1/2">
-      <h4 class="m-4 font-medium">
-        Una persona pronta ad affrontare le sfide. Amo ascoltare
-        musica di tutti i generi, <span class="font-semibold">praticare sport all'aperto,
-          leggere, scrivere e imparare</span>.
-        Viaggiare per confrontarmi con nuove culture, lingue e sapori. Per conoscermi meglio, clicca il pulsante qui sotto. Verrai indirizzato alla mia pagina di Europass, dove, se vorrai, potrai vedere le esperienze e gli studi che ho svolto.
-      </h4>
-      <a href="https://europa.eu/!UP46WC" target="_blank"><button
-        class="bg-white hover:bg-green-200 text-green-900
+    <div class="bg-green-900">
+      <h1 class="text-white text-left pl-3 md:pl-20 pt-20 text-3xl">
+        <span class="font-semibold">Lucian Cucu</span>
+      </h1>
+      <h2 class="text-white text-left pl-2 md:pl-20 pt-20">
+        Tirocinio presso <span class="font-semibold">REVAS
+          Societa Benefit</span> Trento.
+        Master in "Esperto in Innovazione Tecnologica applicata".
+      </h2>
+      <h3 class="text-white text-left pl-1.5 md:pl-20 pb-20">
+        Laureato in <span class="font-semibold">Sociologia</span>
+        presso <span class="font-semibold">l'Università di Trento</span>.
+      </h3>
+    </div>
+    <div class="flex-row flex-shrink md:flex">
+      <div class="md:w-1/2">
+        <h4 class="m-4 font-medium">
+          Una persona pronta ad affrontare le sfide. Amo ascoltare
+          musica di tutti i generi, <span class="font-semibold">praticare sport all'aperto,
+            leggere, scrivere e imparare</span>.
+          Viaggiare per confrontarmi con nuove culture, lingue e sapori. Per conoscermi meglio, clicca il pulsante qui sotto. Verrai indirizzato alla mia pagina di Europass, dove, se vorrai, potrai vedere le esperienze e gli studi che ho svolto.
+        </h4>
+        <a href="https://europa.eu/!UP46WC" target="_blank"><button
+          class="bg-white hover:bg-green-200 text-green-900
             font-bold py-1 px-4 rounded-full m-4
             border border-green-500 text-sm text-left"
-      >
-        Scopri di più su di me
-      </button></a>
-    </div>
+        >
+          Scopri di più su di me
+        </button></a>
+      </div>
 
-    <div class="hidden md:block md:w-1/2 p-4">
-      <img
-        class="w-full"
-        src="https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-      >
-      <img>
+      <div class="hidden md:block md:w-1/2 p-4">
+        <img
+          class="w-full"
+          src="https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        >
+        <img>
+      </div>
+    </div>
+    <div class="flex-row flex-shrink md:flex">
+      <div class="md:w-1/2 p-4">
+        <img
+          class="w-full"
+          src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW91bnRhaW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        >
+      </div>
+      <div class="md:w-1/2 text-right">
+        <h5 class="m-4 font-medium">
+          In un mondo così incerto, <span class="font-semibold">l'adattamento </span>è la chiave per avere successo. Siamo come gli animali, viviamo in un habitat che cambia continuamente ad un ritmo elevato. Stiamo affrontando grandi sfide, come il <span class="font-semibold">cambiamento climatico, l'inquinamento, la povertà e la fame.</span>Per questo motivo le nostre abitudini devono cambiare, ora. Adesso, per un mondo più bello e felice, nel presente ed in futuro. Se sei interessato a conoscermi meglio, dai un'occhiata ai miei studi e alle mie esperienze. Cliccando il bottone qui sotto, verrai renderizzato al mio <span class="font-semibold">profilo Europass in lingua inglese</span>.
+        </h5>
+        <h6 class="text-xs text-gray m-4 font-medium">
+          Scopri di più su di me, cliccando il pulsante qui sotto.
+        </h6>
+        <a
+          class="bg-white hover:bg-green-200 text-green-900 font-bold py-1 px-4 rounded-full m-4 border border-green-500 text-sm text-left"
+          href="https://europa.eu/!Fm66rR"
+          target="_blank"
+        >
+          Di più su di me
+        </a>
+      </div>
     </div>
   </div>
-  <div class="flex-row flex-shrink md:flex">
-    <div class="md:w-1/2 p-4">
-      <img
-        class="w-full"
-        src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW91bnRhaW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-      ></img>
-    </div>
-    <div class="md:w-1/2 text-right">
-      <h5 class="m-4 font-medium">
-        In un mondo così incerto, <span class="font-semibold">l'adattamento </span>è la chiave per avere successo. Siamo come gli animali, viviamo in un habitat che cambia continuamente ad un ritmo elevato. Stiamo affrontando grandi sfide, come il <span class="font-semibold">cambiamento climatico, l'inquinamento, la povertà e la fame.</span>Per questo motivo le nostre abitudini devono cambiare, ora. Adesso, per un mondo più bello e felice, nel presente ed in futuro. Se sei interessato a conoscermi meglio, dai un'occhiata ai miei studi e alle mie esperienze. Cliccando il bottone qui sotto, verrai renderizzato al mio <span class="font-semibold">profilo Europass in lingua inglese</span>.
-      </h5>
-      <h6 class="text-xs text-gray m-4 font-medium">
-        Scopri di più su di me, cliccando il pulsante qui sotto.
-      </h6>
-      <a
-        class="bg-white hover:bg-green-200 text-green-900 font-bold py-1 px-4 rounded-full m-4 border border-green-500 text-sm text-left"
-        href="https://europa.eu/!Fm66rR"
-        target="_blank"
-      >
-        Di più su di me
-      </a>
-    </div>
-  </div>
-</div>
 </template>
